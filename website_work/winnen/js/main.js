2fff33491444101427bd388dcb2024f8649f10f1
shinhee <tlsgml656@gmail.com>
2022-12-06 00:04:57 +0900

    수정

--- website_work/winnen/js/main.js
+++ website_work/winnen/js/main.js
@@ -1,20 +1,20 @@
 //애니메이션
 let text = $("header h1").text();
-
+//  console.log(text)
 let split = text.split("").join("</span><span>");
 split = "<span>" + split + "</span>";
-$("header h1").html(split);
+$("header h1").html(split)
 
 $(window).scroll(function(){
-    let scrolTop = $(window).scrollTop();
+  let scrollTop = $(window).scrollTop();
 
-    if(scrolTop>=0){
-        $("header").addClass("show");
-    }
-    for(let i=1; i<=3; i++){
-        if(scrolTop >= $("#section"+i).offset().top - $(window).height()/2){
-            $("#section"+i).addClass("show");
-        }
-    }
+  if(scrollTop>=0){
+      $("header").addClass("show");
+  }
+  for(let i=1; i<=2; i++){
+      if(scrollTop >= $("#section"+i).offset().top - $(window).height()/2){
+          $("#section"+i).addClass("show");
+      }
+  }
     
 })
