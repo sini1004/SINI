378b9da696cc2d9d0dc8e18bd66d1e9174be87c3
shinhee <tlsgml656@gmail.com>
2022-12-09 14:50:20 +0900

    포트폴리오 작업

--- website_work/winnen/js/main.js
+++ website_work/winnen/js/main.js
@@ -9,12 +9,12 @@ $(window).scroll(function(){
   let scrollTop = $(window).scrollTop();
 
   if(scrollTop>=0){
-      $("header").addClass("show");
+    $("header").addClass("show");
   }
   for(let i=1; i<=2; i++){
-      if(scrollTop >= $("#section"+i).offset().top - $(window).height()/2){
-          $("#section"+i).addClass("show");
-      }
+    if(scrollTop >= $("#section"+i).offset().top - $(window).height()/2){
+      $("#section"+i).addClass("show");
+    }
   }
     
 })
