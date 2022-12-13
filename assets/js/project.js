b843492a481de560ce2344d68cda13b51748d92a
sini1004 <tlsgml656@gmail.com>
2022-12-13 15:17:51 +0900

    프로젝트 테스트

--- assets/js/project.js
+++ assets/js/project.js
@@ -1,33 +1,33 @@
 window.onload = function () {
-  startLoadFile();
+  //startLoadFile();
 };
 
-function startLoadFile(){
-  $.ajax({
-      url: '../project.json',
-      type: 'GET',
-      dataType : 'json',
-      success : function (data) {
-          createProject(data)
-      }
-  });
-}
+// function startLoadFile(){
+//   $.ajax({
+//       url: '../project.json',
+//       type: 'GET',
+//       dataType : 'json',
+//       success : function (data) {
+//           createProject(data)
+//       }
+//   });
+// }
 
-// JSON 포멧 데이터 처리
-function createProject(objImageInfo) {
-  var project = objImageInfo.project;
-  var strDOM = "";
-  for (var i = 0; i < project.length; i++) {
-      // N번째 이미지 정보를 구하기
-      var image = project[i];
+// // JSON 포멧 데이터 처리
+// function createProject(objImageInfo) {
+//   var project = objImageInfo.project;
+//   var strDOM = "";
+//   for (var i = 0; i < project.length; i++) {
+//       // N번째 이미지 정보를 구하기
+//       var image = project[i];
 
-      //  N번째 이미지 패널을 생성
-      //strDOM += '<div class="image_panel">';
-      //strDOM += '    '<img src="' + image.url + '">';
-      //strDOM += '    '<p class="title">' + image.title + ''</p>';
-      //strDOM += ''</div>';
-  }
-  // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
-  var $imageContainer = $("#image_container");
-      $imageContainer.append(strDOM);
-}
+//       //  N번째 이미지 패널을 생성
+//       //strDOM += '<div class="image_panel">';
+//       //strDOM += '    '<img src="' + image.url + '">';
+//       //strDOM += '    '<p class="title">' + image.title + ''</p>';
+//       //strDOM += ''</div>';
+//   }
+//   // 이미지 컨테이너에 생성한 이미지 패널들을 추가하기
+//   var $imageContainer = $("#image_container");
+//       $imageContainer.append(strDOM);
+// }
