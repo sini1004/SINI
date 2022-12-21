59247d3fcc25b49f35bf010b83db2a263ead07ed
shinhee <tlsgml656@gmail.com>
2022-12-21 18:14:15 +0900

    오투요소수 작업

---
+++ website_work/o2urea/js/main.js
@@ -0,0 +1,20 @@
+//애니메이션
+let text = $("header h1").text();
+//  console.log(text)
+let split = text.split("").join("</span><span>");
+split = "<span>" + split + "</span>";
+$("header h1").html(split)
+
+$(window).scroll(function(){
+  let scrollTop = $(window).scrollTop();
+
+  if(scrollTop>=0){
+    $("header").addClass("show");
+  }
+  for(let i=1; i<=1; i++){
+    if(scrollTop >= $("#section"+i).offset().top - $(window).height()/2){
+      $("#section"+i).addClass("show");
+    }
+  }
+    
+})
