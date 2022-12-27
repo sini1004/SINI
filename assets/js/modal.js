c4d568e3aca2cd9b296ab7ff1ec2190121f9c7ad
shinhee <tlsgml656@gmail.com>
2022-12-27 18:22:42 +0900

    모달 수정 중

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,8 +1,5 @@
-// Modal을 가져옵니다.
-
 
 var modals = document.getElementsByClassName("modal");
-// Modal을 띄우는 클래스 이름을 가져옵니다.
 var btns = document.getElementsByClassName("box");
 
 // Modal을 닫는 close 클래스를 가져옵니다.
@@ -17,24 +14,24 @@ function Modal(num) {
         modals[num].style.display = "block";
         console.log(num);
     };
- 
+    
     // <span> 태그(X 버튼)를 클릭하면 Modal이 닫습니다.
     spanes[num].onclick = function() {
         modals[num].style.display = "none";
     };
   };
 }
- 
+
 // 원하는 Modal 수만큼 Modal 함수를 호출해서 funcs 함수에 정의합니다.
 for(var i = 0; i < btns.length; i++) {
   funcs[i] = Modal(i);
 }
- 
+
 // 원하는 Modal 수만큼 funcs 함수를 호출합니다.
 for(var j = 0; j < btns.length; j++) {
   funcs[j]();
 }
- 
+
 // Modal 영역 밖을 클릭하면 Modal을 닫습니다.
 window.onclick = function(event) {
   if (event.target.className == "modal") {
